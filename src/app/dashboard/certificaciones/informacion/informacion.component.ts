9d96812380de1d5a7b9cb12fead5076a3f0e131a
Ruddy Gerardo <ninjaneja@gmail.com>
2025-03-05 15:47:10 -0400

    Added: modal of signatures

--- src/app/dashboard/certificaciones/informacion/informacion.component.ts
+++ src/app/dashboard/certificaciones/informacion/informacion.component.ts
@@ -1,7 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject, model, signal } from '@angular/core';
 import {MatTableModule} from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
 
 export interface PeriodicElement {
   semestre: string;
@@ -24,17 +35,75 @@ const ELEMENT_DATA: PeriodicElement[] = [
   // {position: 10, semestre: 'Neon', promedio: 20.1797, literal: 'Ne', obs:''},
 ];
 
+export interface DialogData {
+  jefe: string;
+  comandante: string;
+}
+
 @Component({
   selector: 'app-informacion',
-  imports: [MatTableModule,MatButtonModule],
+  imports: [MatTableModule,MatButtonModule,MatFormFieldModule, MatInputModule, FormsModule],
   templateUrl: './informacion.component.html',
   styleUrl: './informacion.component.scss'
 })
 export class InformacionComponent {
+  readonly jefe = signal('');
+  readonly comandante = signal('');
+  // readonly name = model('');
+  readonly dialog = inject(MatDialog);
   displayedColumns: string[] = ['position', 'semestre', 'promedio', 'literal', 'obs'];
   dataSource = ELEMENT_DATA;
 
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      data: {jefe: this.jefe(), comandante: this.comandante()},
+      // data: {jefe: '', comandante: ''},
+      width:'400px',
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      // console.log('The dialog was closed');
+      if (result) {
+        this.jefe.set(result.jefe);
+        this.comandante.set(result.comandante);
+      }
+    });
+  }
   print(){
     window.print();
+  }
+}
+
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: 'dialog-overview-example-dialog.html',
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+  ],
+})
+export class DialogOverviewExampleDialog {
+  readonly dialogRef = inject(MatDialogRef<DialogOverviewExampleDialog>);
+  readonly data = inject<DialogData>(MAT_DIALOG_DATA);
+  // readonly jefe = model(this.data.jefe);
+  // readonly comandante = model(this.data.comandante);
+  jefe = this.data.jefe;
+  comandante = this.data.comandante
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  save(){
+    console.log(this.jefe);
+    console.log(this.comandante);
+    this.dialogRef.close({ jefe: this.jefe, comandante: this.comandante });
   }
 }
